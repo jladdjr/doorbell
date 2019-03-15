c94b8ac8b479510102484df7b9c17f0f5f614744
Jim Ladd <jladd@redhat.com>
2019-03-15 13:09:35 -0700

    Map addresses to names

--- doorbell.py
+++ doorbell.py
@@ -6,6 +6,9 @@ from time import sleep, time
 
 debug = False
 
+address_to_name_map = {'192.168.1.2': 'Jim',
+                       '192.168.1.3': 'Stephanie'}
+
 class State:
     def __init__(self, active=False, inactive_time=None):
         self.active = active
@@ -30,8 +33,11 @@ def ping_devices(addresses):
 
     return (active_addresses, inactive_addresses)
 
+def get_identity(address):
+    return address_to_name_map.get(address, address)
+
 if __name__ == '__main__':
-    addresses = ['192.168.1.{}'.format(i) for i in range(200,211)]
+    addresses = ['192.168.1.{}'.format(i) for i in range(2,4)]
 
     state = {a: State(False, time()) for a in addresses}
     last_check = time()
@@ -48,12 +54,13 @@ if __name__ == '__main__':
             print('Resuming checks after sleep.. \o/')
             last_check = time()
             continue
+        last_check = time()
 
         for device in inactive_devices:
             if state[device].active:
                 state[device].active = False
                 state[device].inactive_time = time()
-                print('{} went away..'.format(device))
+                print('{} went away..'.format(get_identity(device)))
 
         for device in active_devices:
             if not state[device].active:
@@ -65,11 +72,9 @@ if __name__ == '__main__':
                 else:
                     time_away = (time() - state[device].inactive_time) // 60  # In minutes
                     note_on_timing = '(away {0} minutes)'.format(time_away)
-                print('{0} came online {1}'.format(device, note_on_timing))
+                print('{0} came online {1}'.format(get_identity(device), note_on_timing))
 
                 if time_away > 30:
-                    os.system('notify {} is home'.format(device))
-                    os.system('text_steph')
-                    os.system('text_jim')
+                    os.system('notify {} is home'.format(get_identity(device)))
 
         sleep(5)
